74fc62fd114db37c45496548a77b551fdbf1a859
徐伟新 <564875789@qq.com>
2025-06-24 09:15:26 +0800

    Initial Commit

---
+++ ZDBleTool/ZDBleTool.h
@@ -0,0 +1,18 @@
+//
+//  ZDBleTool.h
+//  ZDBleTool
+//
+//  Created by 徐伟新 on 2025/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZDBleTool.
+FOUNDATION_EXPORT double ZDBleToolVersionNumber;
+
+//! Project version string for ZDBleTool.
+FOUNDATION_EXPORT const unsigned char ZDBleToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZDBleTool/PublicHeader.h>
+
+
